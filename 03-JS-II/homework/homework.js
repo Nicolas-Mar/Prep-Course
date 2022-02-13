d6cba3377a7cc6db5b775572b5d72085ac407ad8
Nicolas-Mar <akoru66@gmail.com>
2022-02-13 18:56:35 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -135,6 +135,8 @@ function fizzBuzz(numero) {
     return "buzz";
   } else if (divisible3 === 0) {
     return "fizz";
+  } else {
+    return numero;
   }
 }
 
@@ -166,6 +168,18 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+if (numero < 2) {
+  return false;
+}
+if (numero === 2) {
+  return true;
+}
+for (let i = 2; i < numero; i++) {
+  if (numero % i === 0) {
+    return false;
+  }
+}
+  return true;
 }
 
 function esVerdadero(valor){
